a4e7fbd78fda7c212ca21aad8568cf0749c4593f
Alan Lee <alleetw101@gmail.com>
2020-10-21 23:30:10 -0400

    learning additions

--- main.cpp
+++ main.cpp
@@ -6,11 +6,36 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main() {
     // Basic output
     std::cout << 1;
     std::cout << "\n";
     std::cout << "Second line\n";
+    
+    // Basic input and output
+    int number = 5;
+    std::cout << "Enter a number: ";
+    // std::cin >> number;
+    std::cout << "Five times your number is: " << number * 5 << "\n";
+    
+    // Variable types
+    bool boolean = true;
+    char character = 'c';
+    float single_precision = 3.14;
+    double double_precision = 2.718;
+    
+    std::cout << single_precision * double_precision << "\n";
+    
+    if(boolean) {
+        std::cout << character << "\n";
+    } else {
+        std::cout << "False" << "\n";
+    }
+    
+    std::string result = (boolean) ? "True" : "False";
+    std::cout << result << "\n";
+    
     return 0;
 }
