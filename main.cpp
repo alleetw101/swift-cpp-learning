b8b1313be1fdebef72ace0ddacdd044a8c994a6e
Alan Lee <alleetw101@gmail.com>
2020-10-22 11:02:38 -0400

    additions

--- main.cpp
+++ main.cpp
@@ -28,6 +28,7 @@ int main() {
     
     std::cout << single_precision * double_precision << "\n";
     
+    // If expression (w/ shortened form)
     if(boolean) {
         std::cout << character << "\n";
     } else {
@@ -36,6 +37,36 @@ int main() {
     
     std::string result = (boolean) ? "True" : "False";
     std::cout << result << "\n";
+    
+    // sizeof()
+    int char_size = sizeof(character);
+    int bool_size = sizeof(boolean);
+    int int_size = sizeof(number);
+    int float_size = sizeof(single_precision);
+    int double_size = sizeof(double_precision);
+    
+    std::cout << "Char size: " << char_size;
+    std::cout << "\nBool size: " << bool_size;
+    std::cout << "\nInt size: " << int_size;
+    std::cout << "\nFloat size: " << float_size;
+    std::cout << "\nDouble size: " << double_size << "\n";
+    
+    // Loops
+    for(int x=10; x<20; x++) {
+        std::cout << x << " ";
+        if(x==19) {
+            std::cout << "\n";
+        }
+    } // Prints 10-19
+    
+    do {
+        number++;
+        std::cout << number << " ";
+        if(number==10) {
+            std::cout << "\n";
+        }
+    } while(number<10); // Prints 6-10
+        
     
     return 0;
 }
