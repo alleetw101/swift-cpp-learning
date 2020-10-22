e2baa892269dcc70e6fa13ffb83d6b0ac443afe4
Alan Lee <alleetw101@gmail.com>
2020-10-21 21:57:34 -0400

    Initial Commit

---
+++ Learning C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learning C++
+//
+//  Created by Alan Lee on 10/21/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
