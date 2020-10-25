afab923c7e4d358acc96d4e0d91d46fb993840ae
Alan Lee <alleetw101@gmail.com>
2020-10-25 17:36:16 -0400

    added correct implementation of the quicksort algorithm

--- Algorithms/array_sorting_algorithms.swift
+++ Algorithms/array_sorting_algorithms.swift
@@ -5,51 +5,50 @@
 //  Created by Alan Lee on 10/24/20.
 //
 
-import Foundation
-
 // Quicksort
-// Core sort function
-func quicksort(array: [Double]) -> [Double] {
-  
-  if array.count <= 1 {
-    return array
+/// Sorts an array from least to greatest using the quicksort implementation
+/// - Parameters:
+///   - array: An array of Double elements  to be sorted
+///   - start: Start index of elements to be sorted
+///   - end: End index of elements to be sorted
+///   - type: A string to specify index to use when partitioning the array ("first", "random", "middle"). Default implementation uses last index.
+func quicksort(array: inout [Double], start: Int, end: Int, type: String) {
+  if start < end {
+    let partition_index = quicksort_partition(array: &array, start: start, end: end, type: type)
+    
+    quicksort(array: &array, start: start, end: partition_index - 1, type: type)
+    quicksort(array: &array, start: partition_index + 1, end: end, type: type)
   }
-  
-  var return_array = [Double]()
-  
-  let pivot = quicksort_get_pivot(array: array, type: "first")
-  return_array = (quicksort(array: quicksort_partition(array: array, pivot: pivot).lower) + [pivot] +
-                    quicksort(array: quicksort_partition(array: array, pivot: pivot).higher))
-  
-  return return_array
 }
 
-// TODO: Use index instead of value for repetition cases
-func quicksort_get_pivot(array: [Double], type: String = "mean") -> Double {
-  var pivot_value: Double
-  
+/// Partitions elements of an array around a pivot element between specified indices
+/// - Parameters:
+///   - array: An array of Double elements to be partitioned
+///   - start: Start index of elements to be partitioned
+///   - end: End index of elements to be partitioned
+///   - type: A string to specify index to use when determining pivot element ("first", "random", "middle"). Default implementation uses last index.
+/// - Returns: The index of the pivot element where lower indices contain elements lower than the pivot element and higher indices contain elements greater than the pivot element
+func quicksort_partition(array: inout [Double], start: Int, end: Int, type: String) -> Int {
   switch type {
-  case "first":
-    pivot_value = array[0]
-  case "last":
-    pivot_value = array[array.count - 1]
-  default:
-    pivot_value = array[(array.count - 1) / 2]
+    case "first":
+      array.swapAt(start, end)
+    case "random":
+      array.swapAt(Int.random(in: start...end), end)
+    case "middle":
+      array.swapAt((end + start) / 2, end)
+    default:
+      break
   }
   
-  return pivot_value
-}
-
-func quicksort_partition(array: [Double], pivot: Double) -> (lower: [Double], higher: [Double]) {
-  var lower = [Double]()
-  var upper = [Double]()
+  var x = start
+  let pivot = array[end]
   
-  for index in 0...array.count - 1 {
+  for index in start...end {
     if array[index] < pivot {
-      lower.append(array[index])
-    } else if array[index] != pivot {
-      upper.append(array[index])
+      array.swapAt(x, index)
+      x += 1
     }
   }
-  return (lower, upper)
+  array.swapAt(x, end)
+  return x
 }
