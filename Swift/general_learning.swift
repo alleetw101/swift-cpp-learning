a58e043fc0a4e583fef6c0c834ddf30abff1212f
Alan Lee <alleetw101@gmail.com>
2020-10-24 21:25:10 -0400

    initial swift file commits

---
+++ Swift/general_learning.swift
@@ -0,0 +1,12 @@
+//
+//  general_learning.swift
+//  Swift
+//
+//  Created by Alan Lee on 10/24/20.
+//
+
+import Foundation
+
+func general_learning() {
+  print("test")
+}
---
+++ main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift
+//
+//  Created by Alan Lee on 10/24/20.
+//
+
+import Foundation
+
+print("Hello, World!")
+general_learning()
+
